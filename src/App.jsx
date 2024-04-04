7bb862c4e6877329a138909612ffa4ab3c7c7685
MacBooK Pro <holamarialazaro@gmail.com>
2024-04-04 12:43:28 +0200

    Estructura inicial creada

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Home } from "./pages/Home/Home";
+import "./App.css";
+import { Footer } from "./components/Footer/Footer";
+import { Body } from "./pages/Body/Body";
+import { Header } from "./components/Header/Header";
+
+
+function App() {
+  return (
+    <>
+      <Header />
+       <Body />
+      <Footer />
+    
+    </>
+  );
+}
+
+export default App;
