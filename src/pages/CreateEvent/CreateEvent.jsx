f397a3180c235dd22f58e1b7af2c788258978bc8
MacBooK Pro <holamarialazaro@gmail.com>
2024-04-11 19:41:48 +0200

    Confirmar asistencia eventos funciona

--- src/pages/CreateEvent/CreateEvent.jsx
+++ src/pages/CreateEvent/CreateEvent.jsx
@@ -7,99 +7,71 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useSelector } from "react-redux";
-import { userData1 } from "../userSlice";
 
 export const CreateEvent = () => {
     const dataevent = useLocation(); // Obtiene la ubicación actual
-    const { eventId } = useParams(); // Obtiene el ID del evento de la URL
     const navigate = useNavigate();
-    const [eventData, setEventData] = useState({
-      user_id: "",
-      artist_id: "",
-      date: "",
-      location: ""
-    });
+    const [artist_id, setArtistId] = useState("");
     const [registrationSuccess, setRegistrationSuccess] = useState(false);
-console.log(dataevent.state.eventData
-    )
-    useEffect(() => {
-      if (dataevent.state && dataevent.state.eventData) {
-        const { date, location, user_id } = dataevent.state.eventData;
-        setEventData({
-          ...eventData,
-          user_id,
-          date,
-          location
-        });
-      }
-    }, [location.state]);
 
     const handleSubmit = async (event) => {
-      event.preventDefault();
-      try {
-        await createEvent(eventData);
-        setRegistrationSuccess(true);
-        setTimeout(() => {
-          navigate("/");
-        }, 5000);
-      } catch (error) {
-        console.error("Error al registrar el evento:", error);
-      }
+        event.preventDefault();
+        try {
+            await createEvent({
+                user_id: dataevent.state.eventData.user_id,
+                artist_id: artist_id,
+                date: dataevent.state.eventData.date,
+                location: dataevent.state.eventData.location
+            });
+            setRegistrationSuccess(true);
+            setTimeout(() => {
+                navigate("/");
+            }, 5000);
+        } catch (error) {
+            console.error("Error al registrar el evento:", error);
+        }
     };
 
     const inputHandler = (event) => {
-      setEventData({
-        ...eventData,
-        [event.target.name]: event.target.value
-      });
+        setArtistId(event.target.value);
     };
 
     return (
-      <>
-        <h1 className="tituloevento">Registro de Evento</h1>
-        <div className="registro-evento">
-          {registrationSuccess && (
-            <div className="registration-success-popup">
-              <p>¡Evento registrado correctamente!</p>
-            </div>
-          )}
+        <>
+            <h1 className="tituloevento">¿Quieres asistir este Evento?</h1>
+            <div className="registro-evento">
+                {registrationSuccess && (
+                    <div className="registration-success-popup">
+                        <p>¡Evento registrado correctamente!</p>
+                    </div>
+                )}
 
-          <Form onSubmit={handleSubmit}>
-            <Row className="mb-2">
-           
-              <Form.Group as={Col} md="12" controlId="validationCustom02">
-                <CustomInput
-                  type="datetime-local"
-                  text="Fecha y Hora"
-                  name="date"
-                  value={dataevent.state.eventData.date}
-                  readOnly
-                />
-              </Form.Group>
-              <Form.Group as={Col} md="12" controlId="validationCustom03">
-                <CustomInput
-                  placeholder="Ubicación"
-                  type="text"
-                  name="location"
-                  value={eventData.location}
-                  readOnly
-                />
-              </Form.Group>
-              <Form.Group as={Col} md="12" controlId="validationCustom04">
-                <CustomInput
-                  placeholder="ID del Artista"
-                  type="number"
-                  name="artist_id"
-                  value={eventData.artist_id}
-                  onChange={inputHandler}
-                />
-              </Form.Group>
-            </Row>
-            <Button type="submit">Registrar Evento</Button>
-          </Form>
-        </div>
-      </>
+                <Form onSubmit={handleSubmit}>
+                    <Row className="mb-2">
+                  <h1> {dataevent.state.location} </h1>
+                    </Row>
+                    <Row className="mb-2">
+                        <Col md="12">
+                   <h2> {dataevent.state.date}</h2>
+                        </Col>
+                    </Row>
+                    <Row className="mb-2">
+               
+                    </Row>
+                    <Row className="mb-2">
+               <h3> Tu ID de Usuario es {dataevent.state.user_id}</h3>
+                    </Row>
+                    <CustomInput
+                                placeholder="ID del Artista"
+                                type="number"
+                                value={artist_id}
+                                handler={inputHandler}
+                            />
+
+                    <Button type="submit">CONFIRMAR ASISTENCIA</Button>
+                </Form>
+            </div>
+        </>
     );
 };
 
